2272791e535b3c9950fc23c646fdf1eb961e2106
kbzcai <67303930+kbzcai@users.noreply.github.com>
2020-08-09 15:41:16 +0800

    1

---
+++ zipkin/src/main/java/com/cyj/ZipKinApplication.java
@@ -0,0 +1,13 @@
+package com.cyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class,args);
+    }
+}
